5f102e1c2132cb4a652a55e5803ee3c14f7850fc
Dennis Munene <munenedennis868@gmail.com>
2024-03-10 07:20:55 +0300

    Add files via upload

---
+++ models/__init__.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""__init__ magic method for mode0ls dir"""
+from models.engine.file_storage import FileStorage
+
+
+storage = FileStorage()
+storage.reload()
+
+"""adonijah/betty"""
